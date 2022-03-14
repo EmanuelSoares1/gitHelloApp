3b272cc60dd1e0e32f6c4a7939d31509b803b429
Emanuel Soares1 <emanuelsoares.eos@gmail.com>
2022-03-14 10:08:37 -0300

    Adicionado GitHelloApp com a main()  - v1.0

---
+++ src/main/java/com/br/eti/emanuel/githelloapp/GitHelloApp.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.eti.emanuel.githelloapp;
+
+/**
+ *
+ * @author devsys-a
+ */
+public class GitHelloApp {
+    public static void main(String[] args) {
+        
+    }
+}
